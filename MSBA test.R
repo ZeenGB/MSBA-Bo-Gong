2877daa4362098011da2b0ea2563453aa275b7d9
ZeenGB <zegi3255@gmail.com>
2021-08-18 15:10:56 -0400

    Done!

--- MSBA test.R
+++ MSBA test.R
@@ -1,1 +1,16 @@
 x<-1:10
+install.packages(c("car", "ggplot2"))
+library(car)
+plot(mtcars$disp, mtcars$mpg)
+abline(lm(mpg ~ disp, data = mtcars))
+library(ggplot2)
+ggplot(data = mtcars, 
+       mapping = aes(x = disp, y = mpg)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+theme_minimal()
+install.packages('plotly')
+plotly::ggplotly(my_plot)
+st_joe_land <- read.delim(file = "stJoeLand.csv", sep = ",")
+
+st_joe_land <- read.csv(file = "stJoeLand.csv")
